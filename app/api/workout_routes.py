94a9922376c3b5a7fb7316c6a71580966334071e
Clay <crp2529@gmail.com>
2020-11-24 15:08:15 -0800

    create route and create workout posting to db!

--- app/api/workout_routes.py
+++ app/api/workout_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required
-from app.models import Workout
+from app.models import Workout, db
 
 workout_routes = Blueprint('workouts', __name__)
 
@@ -17,3 +17,27 @@ def workouts():
 def workout(id):
     workout = Workout.query.get(id)
     return workout.to_dict()
+
+
+@workout_routes.route('/custom', methods=['POST'])
+@login_required
+def add_custom_workout():
+    data = request.json
+    print(data)
+    errors = []
+    workout_photos = data['workout_photos']
+    time = data['time']
+    custom_workout = Workout(
+        name=data['name'],
+        description=data['description'],
+        workout_photos=f'{workout_photos}',
+        time=f'{time}',
+        workout_date=data['workout_date'],
+        user_id=data['user_id'],
+        route_id=data['route_id'],
+    )
+    if errors:
+        return jsonify(errors)
+    db.session.add(custom_workout)
+    db.session.commit()
+    return jsonify(data)
