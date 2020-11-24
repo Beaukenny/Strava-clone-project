2b0bce7e61d4a8a2bde56e870023e310463d2a37
Clay <crp2529@gmail.com>
2020-11-24 13:26:44 -0800

    create custom route, added need to adjust

--- app/api/route_routes.py
+++ app/api/route_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import Route
+import json
+import requests
 
 route_routes = Blueprint('routes', __name__)
 
@@ -18,3 +20,25 @@ def routes():
 def route(id):
     route = Route.query.get(id)
     return route.to_dict()
+
+
+@route_routes.route('/custom', methods=['POST'])
+@login_required
+def add_custom_route():
+    data = request.json
+    errors = []
+    route_preview = data['route_preview']
+    route_data = json.dumps(data['route_data'])
+    custom_route = Event(
+        name=data['name'],
+        host_id=data['host_id'],
+        route_preview=f'{route_preview}',
+        route_data=f'{route_data}',
+        starting_point=data['starting_point'],
+        offroad=data['offroad'],
+    )
+    if errors:
+        return jsonify(errors)
+    db.session.add(custom_route)
+    db.session.commit()
+    return jsonify(data)
