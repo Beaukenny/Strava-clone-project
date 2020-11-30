c802a755d9972b309dce0e0cfeb4e205bd9bdde4
Peter <southpeter77@gmail.com>
2020-11-30 01:18:46 -0500

    added textfield

--- react-app/src/components/workout/Workout.js
+++ react-app/src/components/workout/Workout.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
@@ -21,6 +21,8 @@ import { apiUrl } from '../../config'
 import Grid from '@material-ui/core/Grid';
 import DirectionsBikeIcon from '@material-ui/icons/DirectionsBike';
 import DirectionsWalkIcon from '@material-ui/icons/DirectionsWalk';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
 
 
 
@@ -56,19 +58,18 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Workout() {
     const classes = useStyles();
-    const {userId, routeId} = useParams()
+    const { userId, routeId } = useParams()
     const [userData, setuserData] = useState("")
     const [routeData, setrouteData] = useState("")
-    let timestamp
-    useEffect(()=> {
-        async function getData () {
+    // time = time.split("-0500 (Eastern Standard Time")
+    useEffect(() => {
+        async function getData() {
             const getUser = await fetch(`${apiUrl}/users/${Number.parseInt(userId)}`)
             const getRoute = await fetch(`${apiUrl}/routes/${Number.parseInt(routeId)}`)
             const user = await getUser.json()
             const route = await getRoute.json()
             setuserData(user)
             setrouteData(route)
-            timestamp = new Date()
         }
         getData();
     }, [])
@@ -76,89 +77,138 @@ export default function Workout() {
     return (
         <>
             <Typography variant="h3" component="h3" color="primary" align="center">Edit Workout Details:</Typography>
-            <button onClick={()=>console.log(userData, routeData)}>bbbbbbbb</button>
+            <button onClick={() => console.log(typeof (time))}>bbbbbbbb</button>
             <h1>{userData.username}</h1>
             <Paper className={classes.paper}>
-            <Card className={classes.root}>
-            <CardHeader
-                avatar={
-                    <Avatar
-                        aria-label="recipe"
-                        // src
-                    >
-                    </Avatar>
-                }
+                <Card className={classes.root}>
+                    <CardHeader
+                        avatar={
+                            <Avatar
+                                aria-label="recipe"
+                            // src
+                            >
+                            </Avatar>
+                        }
 
-                title={userData.username}
-                // subheader={date}
-            />
-            <Typography style={{marginLeft:"1em"}}variant="h5" component="h5">{routeData.name}</Typography>
+                        title={userData.username}
 
-            <Grid container>
-            <Grid item xs={3}
-            align="center">
+                    />
+                    <Typography style={{ marginLeft: "1em" }} variant="h5" component="h5">{routeData.name}</Typography>
+
+                    <Grid container>
+                        <Grid item xs={3}
+                            align="center">
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6"
+                                component="h6"
+                                align="left"
+                                style={{ color: "gray" }}
+                            >Distance
+              </Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6"
+                                component="h6"
+                                align="left"
+                                style={{ color: "gray" }}
+                            >Elevation Gain
+              </Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6"
+                                component="h6"
+                                align="left"
+                                style={{ color: "gray" }}
+                            >Time
+              </Typography>
+                        </Grid>
+                        <Grid item xs={3} align="center">
+                            {routeData.travelingMode == "BICYCLING" ?
+                                <DirectionsBikeIcon fontSize="large"></DirectionsBikeIcon>
+                                : <DirectionsWalkIcon fontSize="large"></DirectionsWalkIcon>}</Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6"
+                                component="h6"
+                                align="left"
+                            >{routeData.totalDistance}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6"
+                                component="h6"
+                                align="left"
+                            >{routeData.totalElevation}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Typography variant="h6"
+                                component="h6"
+                                align="left"
+                            >{routeData.totalDuration}
+                            </Typography>
+                        </Grid>
+
+                    </Grid>
+                    <CardMedia
+                        className={classes.media}
+                        image={routeData.staticImageURL}
+                    />
+                    <CardContent>
+                        <Typography variant="h6" color="textSecondary" component="h6">
+                            About this Route: {routeData.description}
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Grid item xs={12} >
+                    <Typography
+                        variant="h6"
+                        component="h6"
+                        style={{ color: "gray" }}
+                    ></Typography>
+
+                    <TextField
+                        variant="outlined"
+                        validators={['required']}
+                        required
+                        fullWidth
+                        name="name"
+                        label="Workout Name"
+                        id="name"
+
+                    />
                 </Grid>
-                <Grid item xs={3}>
-              <Typography variant="h6"
-                component="h6"
-                align="left"
-                style={{ color: "gray" }}
-              >Distance
-              </Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography variant="h6"
-                component="h6"
-                align="left"
-                style={{ color: "gray" }}
-              >Elevation Gain
-              </Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography variant="h6"
-                component="h6"
-                align="left"
-                style={{ color: "gray" }}
-              >Time
-              </Typography>
-            </Grid>
-            <Grid item xs={3} align="center">
-                    {routeData.travelingMode == "BICYCLING" ?
-                        <DirectionsBikeIcon fontSize="large"></DirectionsBikeIcon>
-                        : <DirectionsWalkIcon fontSize="large"></DirectionsWalkIcon>}</Grid>
-            <Grid item xs={3}>
-              <Typography variant="h6"
-                component="h6"
-                align="left"
-              >{routeData.totalDistance}
-              </Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography variant="h6"
-                component="h6"
-                align="left"
-              >{routeData.totalElevation}
-              </Typography>
-            </Grid>
-            <Grid item xs={3}>
-              <Typography variant="h6"
-                component="h6"
-                align="left"
-              >{routeData.totalDuration}
-              </Typography>
-            </Grid>
-
-            </Grid>
-            <CardMedia
-                className={classes.media}
-                image={routeData.staticImageURL}
-            />
-            <CardContent>
-                <Typography variant="h6" color="textSecondary" component="h6">
-                    Description: {routeData.description}
-        </Typography>
-            </CardContent>
-        </Card>
+                <Grid item xs={12}>
+                    <Typography
+                        variant="h6"
+                        component="h6"
+                        style={{ color: "gray" }}
+                    ></Typography>
+                    <TextField
+                        variant="outlined"
+                        fullWidth
+                        multiline
+                        rows={8}
+                        name="description"
+                        label="Workout Description"
+                        id="description"
+                    // value={description}
+                    // onChange={updateProperty(setDescription)}
+                    />
+                </Grid>
+                <TextField
+                    id="filled-number"
+                    label="Total Workout Minutes"
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    variant="filled"
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                >Complete</Button>
             </Paper>
 
         </>
