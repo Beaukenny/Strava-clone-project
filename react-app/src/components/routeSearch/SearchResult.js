8fe51f5b6784e8eb84cac96a9dedcd2b71037a50
Peter <southpeter77@gmail.com>
2020-11-29 21:31:44 -0500

    searching for h1

--- react-app/src/components/routeSearch/SearchResult.js
+++ react-app/src/components/routeSearch/SearchResult.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAllRouteWithCoords } from '../../store/actions/routeSearch';
 import { apiUrl } from '../../config';
 import RouteCard from './RouteCard'
-import Search from "./Searchbar"
+import SearchBar from "./Searchbar"
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import DirectionsWalkIcon from '@material-ui/icons/DirectionsWalk';
@@ -16,6 +16,7 @@ import AddBoxIcon from '@material-ui/icons/AddBox';
 import Tooltip from '@material-ui/core/Tooltip';
 import Paper from '@material-ui/core/Paper';
 import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import {getCertainRoutes} from "./utils"
 const styles = makeStyles((theme) => ({
     paper: {
         maxWidth: 750,
@@ -39,8 +40,12 @@ const SearchResult = () => {
             setData(res)
             setLoad(!load)
         }
+
         if (!coordinates) {
             getAllRoutes();
+        }else {
+            getCertainRoutes(coordinates, setData, setLoad,load)
+            
         }
 
     }, [])
@@ -50,11 +55,11 @@ const SearchResult = () => {
 
     return (
         <>
-            {/*     
-            <button onClick={() => console.log(data.routes[0].staticImageURL)}>RES</button>
+                
+            <button onClick={() => console.log(data.routes[0].starting_point)}>RES</button>
             <button onClick={() => console.log(coordinates)}>COORDINATES</button>
             <button onClick={() => console.log(data)}>data</button>
-            <button onClick={() => console.log(load)}>load</button> */}
+            <button onClick={() => console.log(load)}>load</button>
             <Typography variant="h3" component="h3" color="primary" align="center">Explore Routes:</Typography>
             <Tooltip title={<h2>Create a brand new route</h2>}>
                      <IconButton className="createRouteButtonInSearch">
@@ -67,18 +72,37 @@ const SearchResult = () => {
                     <DirectionsWalkIcon fontSize="large" style={{ color: "gray", marginBottom: '5pt', marginTop: '15pt', marginRight: '25pt' }}></DirectionsWalkIcon>
                     <DirectionsBikeIcon fontSize="large" style={{ color: "gray", marginBottom: '5pt', marginTop: '15pt', marginRight: '12.5pt', marginLeft: "12.5" }}></DirectionsBikeIcon>
                     <DirectionsRunIcon fontSize="large" style={{ color: "gray", marginBottom: '5pt', marginTop: '15pt', marginLeft: '25pt' }}></DirectionsRunIcon>
-                    <Search></Search>
+                    <SearchBar></SearchBar>
                 </Grid>
             </Grid>
-
             <Paper className={classes.paper}>
-                {data.routes.map(each =>
-
+                {data.routes ? data.routes.map(each =>
                     <RouteCard data={each}></RouteCard>
-
-
-
-                )}
+                ): <><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                
+                >NO sssssssssssssssroute</h1><h1
+                stlye={{zIndex:"99" }}
+                >NO sssssssssssssssroute</h1></>}
 
 
             </Paper>
--- react-app/src/components/routeSearch/Searchbar.js
+++ react-app/src/components/routeSearch/Searchbar.js
@@ -35,47 +35,47 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Searchbar() {
-  const classes = useStyles();
+// function Searchbar() {
+//   const classes = useStyles();
 
-  return (
-    <div>
-      <div>
-        <Grid
-          container
-          // spacing={-1}
-          direction="row"
-          justify="space-between"
-          style={{ marginBottom: "20px", fontWeight: "bold" }}
-          alignItems="space-around"
-        >
-          <i class="fas fa-biking fa-3x"></i>
-          <i class="fas fa-hiking fa-3x"></i>
-          <i class="fas fa-running fa-3x"></i>
-        </Grid>
-      </div>
-      <Paper component="form" className={classes.root}>
-        {/* <IconButton className={classes.iconButton} aria-label="menu">
-            <MenuIcon />
-          </IconButton> */}
+//   return (
+//     <div>
+//       <div>
+//         <Grid
+//           container
+//           // spacing={-1}
+//           direction="row"
+//           justify="space-between"
+//           style={{ marginBottom: "20px", fontWeight: "bold" }}
+//           alignItems="space-around"
+//         >
+//           <i class="fas fa-biking fa-3x"></i>
+//           <i class="fas fa-hiking fa-3x"></i>
+//           <i class="fas fa-running fa-3x"></i>
+//         </Grid>
+//       </div>
+//       <Paper component="form" className={classes.root}>
+//         {/* <IconButton className={classes.iconButton} aria-label="menu">
+//             <MenuIcon />
+//           </IconButton> */}
 
-        <InputBase
-          className={classes.input}
-          placeholder="Please Enter Destination"
-          inputProps={{ 'aria-label': 'search google maps' }}
-        />
-        <IconButton type="submit" className={classes.iconButton} aria-label="search">
-          <SearchIcon />
-        </IconButton>
-        <Divider className={classes.divider} orientation="vertical" />
-        {/* <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-            <DirectionsIcon />
-          </IconButton> */}
-      </Paper>
-    </div>
+//         <InputBase
+//           className={classes.input}
+//           placeholder="Please Enter Destination"
+//           inputProps={{ 'aria-label': 'search google maps' }}
+//         />
+//         <IconButton type="submit" className={classes.iconButton} aria-label="search">
+//           <SearchIcon />
+//         </IconButton>
+//         <Divider className={classes.divider} orientation="vertical" />
+//         {/* <IconButton color="primary" className={classes.iconButton} aria-label="directions">
+//             <DirectionsIcon />
+//           </IconButton> */}
+//       </Paper>
+//     </div>
 
-  );
-}
+//   );
+// }
 
 
 const Search = () => {
