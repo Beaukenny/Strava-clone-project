ce35d0723c7b439fb6a9b0e8fa33406cfea12eb5
Beau Kenny <beau.kenny@yahoo.com>
2020-11-29 23:44:35 -0600

    almost done!

--- react-app/src/components/workout/Workout.js
+++ react-app/src/components/workout/Workout.js
@@ -17,6 +17,13 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Paper from '@material-ui/core/Paper';
 import { useParams } from 'react-router-dom';
+import { apiUrl } from '../../config'
+import Grid from '@material-ui/core/Grid';
+import DirectionsBikeIcon from '@material-ui/icons/DirectionsBike';
+import DirectionsWalkIcon from '@material-ui/icons/DirectionsWalk';
+
+
+
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -25,11 +32,12 @@ const useStyles = makeStyles((theme) => ({
 
     },
     root: {
-        maxWidth: 750,
+        maxWidth: 800,
+        maxHeight: 600
     },
     media: {
         height: 0,
-        paddingTop: '56.25%', // 16:9
+        paddingTop: '40%', // 16:9
     },
     expand: {
         transform: 'rotate(0deg)',
@@ -49,36 +57,108 @@ const useStyles = makeStyles((theme) => ({
 export default function Workout() {
     const classes = useStyles();
     const {userId, routeId} = useParams()
-    const [data, setData] = useState("")
-
+    const [userData, setuserData] = useState("")
+    const [routeData, setrouteData] = useState("")
+    let timestamp
     useEffect(()=> {
-        
-    })
+        async function getData () {
+            const getUser = await fetch(`${apiUrl}/users/${Number.parseInt(userId)}`)
+            const getRoute = await fetch(`${apiUrl}/routes/${Number.parseInt(routeId)}`)
+            const user = await getUser.json()
+            const route = await getRoute.json()
+            setuserData(user)
+            setrouteData(route)
+            timestamp = new Date()
+        }
+        getData();
+    }, [])
 
     return (
         <>
             <Typography variant="h3" component="h3" color="primary" align="center">Edit Workout Details:</Typography>
-            <button onClick={()=>console.log(userId, routeId)}>bbbbbbbb</button>
-
+            <button onClick={()=>console.log(userData, routeData)}>bbbbbbbb</button>
+            <h1>{userData.username}</h1>
             <Paper className={classes.paper}>
-                <Card className={classes.root}>
-                    <CardHeader
-                        avatar={
-                            <Avatar aria-label="recipe">
-                            </Avatar>
-                        }
-                        title="Shrimp and Chorizo Paella"
-                        subheader="September 14, 2016"
-                    />
-                    <CardMedia
-                        className={classes.media}
-                    />
-                    <CardContent>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            This
+            <Card className={classes.root}>
+            <CardHeader
+                avatar={
+                    <Avatar
+                        aria-label="recipe"
+                        // src
+                    >
+                    </Avatar>
+                }
+
+                title={userData.username}
+                // subheader={date}
+            />
+            <Typography style={{marginLeft:"1em"}}variant="h5" component="h5">{routeData.name}</Typography>
+
+            <Grid container>
+            <Grid item xs={3}
+            align="center">
+                </Grid>
+                <Grid item xs={3}>
+              <Typography variant="h6"
+                component="h6"
+                align="left"
+                style={{ color: "gray" }}
+              >Distance
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h6"
+                component="h6"
+                align="left"
+                style={{ color: "gray" }}
+              >Elevation Gain
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h6"
+                component="h6"
+                align="left"
+                style={{ color: "gray" }}
+              >Time
+              </Typography>
+            </Grid>
+            <Grid item xs={3} align="center">
+                    {routeData.travelingMode == "BICYCLING" ?
+                        <DirectionsBikeIcon fontSize="large"></DirectionsBikeIcon>
+                        : <DirectionsWalkIcon fontSize="large"></DirectionsWalkIcon>}</Grid>
+            <Grid item xs={3}>
+              <Typography variant="h6"
+                component="h6"
+                align="left"
+              >{routeData.totalDistance}
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h6"
+                component="h6"
+                align="left"
+              >{routeData.totalElevation}
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h6"
+                component="h6"
+                align="left"
+              >{routeData.totalDuration}
+              </Typography>
+            </Grid>
+
+            </Grid>
+            <CardMedia
+                className={classes.media}
+                image={routeData.staticImageURL}
+            />
+            <CardContent>
+                <Typography variant="h6" color="textSecondary" component="h6">
+                    Description: {routeData.description}
         </Typography>
-                    </CardContent>
-                </Card>
+            </CardContent>
+        </Card>
             </Paper>
 
         </>
