c9d142c934b15d9ca376b51b178cdd24fca10658
Beau Kenny <beau.kenny@yahoo.com>
2020-12-01 10:06:27 -0600

    quick fix

--- react-app/src/components/navigator/NavBar.js
+++ react-app/src/components/navigator/NavBar.js
@@ -5,6 +5,9 @@ import TabNav from "./Header/TabNav";
 import ShowLoginNav from "./Header/ShowLoginNav";
 import ShowLogoutNav from "./Header/ShowLogoutNav";
 import LoggedOutTabNav from "./Header/LoggedOutTabNav";
+import { useParams } from "react-router-dom";
+
+
 const Home = () => {
     const [, setForceUpdate] = useState(Date.now());
 
@@ -16,7 +19,7 @@ const Home = () => {
         explore: 3,
         user: 4,
     }
-    
+
     const [isLoggedin, setIsLoggedin] = useState(false);
     const [selectedTab, setSelectedTab] = useState(indexToTabName[page]);
     const [authenticated, setAuthenticated] = useState(false);
@@ -30,9 +33,9 @@ const Home = () => {
     };
 
     console.log((window.location.href).endsWith("create"))
-    
+
     const options = [{"key": 0, "value":"home"}, {"key": 1, "value": "workouts"}, {"key": 2, "value": "routes"}, {"key": 3, "value": "explore"}, {"key": 4, "value": "user"}];
-  
+
     return (
         <div style={{ marginTop: "10vh", marginLeft: "auto", marginRight: "auto", borderBottom: "2px solid lightGrey", display: "grid", position: "fixed block", width: "70vw", height: "100%", maxHeight: "10vh", top: "10vh"}}>
             <Grid style={{ border: "0px solid blue"}} container xs={12}>
